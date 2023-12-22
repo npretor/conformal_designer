ef3c6dd5d7f098e439f17cfd078090e5d908ffc5
Nathan Pretorius <nathan@Nathans-MBP.attlocal.net>
2023-12-22 15:47:28 -0800

    frontend and backend component saving to json

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,12 +1,24 @@
 import os 
-from flask import Flask, render_template, request, redirect, jsonify 
+import json 
+from flask import Flask, render_template, request, redirect, jsonify, Response
 
 app = Flask(__name__)
+
+database = {}
+database["components"]  = {}
 
 @app.route("/")
 def home():
     return render_template('index.html') 
 
+@app.route("/save_components", methods={"POST"})
+def save_components():
+
+    database["components"] = json.loads(request.data) 
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+
 if __name__ == "__main__":
     # print(os.listdir('.')) 
-    app.run(host='0.0.0.0', port=8000, debug=False) 
+    app.run(host='0.0.0.0', port=8000, debug=False)  
